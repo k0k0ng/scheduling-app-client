b301cb7b29ffe7bba74163bbf8e07cfbc5d1d71a
John Rico P. Masadas <masadasjohnrico@gmail.com>
2023-10-18 22:02:51 +0800

    Started creating topnav component

--- src/components/topnav.tsx
+++ src/components/topnav.tsx
@@ -6,34 +6,32 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
 export default function TopNav() {
   return (
-    <>
-      <nav className="flex min-h-[75px] w-full flex-row items-center justify-between border px-20">
-        <div>Overview</div>
-        <div className="flex flex-row gap-x-4">
-          <button>
-            <NotificationsNoneIcon />
-          </button>
+    <nav className="flex min-h-[75px] w-full flex-row items-center justify-between border px-20">
+      <div>Overview</div>
+      <div className="flex flex-row gap-x-4">
+        <button type="button">
+          <NotificationsNoneIcon />
+        </button>
 
-          <select className="border-none bg-transparent">
-            <option className="bg-[#8844DA] p-8 text-center">EN</option>
-            <option className="bg-[#8844DA] p-2 text-center">CH</option>
-            <option className="bg-[#8844DA] p-2 text-center">EU</option>
-          </select>
+        <select className="border-none bg-transparent">
+          <option className="bg-[#8844DA] p-8 text-center">EN</option>
+          <option className="bg-[#8844DA] p-2 text-center">CH</option>
+          <option className="bg-[#8844DA] p-2 text-center">EU</option>
+        </select>
 
-          <button>
-            <CampaignIcon />
-          </button>
+        <button type="button">
+          <CampaignIcon />
+        </button>
 
-          <div className="ml-8 flex flex-col">
-            <span>John Doe</span>
-            <span>Free Account</span>
-          </div>
-
-          <div className="flex items-center">
-            <AccountCircleIcon />
-          </div>
+        <div className="ml-8 flex flex-col">
+          <span>John Doe</span>
+          <span>Free Account</span>
         </div>
-      </nav>
-    </>
+
+        <div className="flex items-center">
+          <AccountCircleIcon />
+        </div>
+      </div>
+    </nav>
   );
 }

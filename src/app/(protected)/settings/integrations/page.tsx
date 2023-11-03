18fd6539390949271c64f830328f67cf74fbe808
James Joshua Ginete <james.ginete10@gmail.com>
2023-11-04 02:50:03 +0800

    Add password manager page: Update label to mobile viewport: Update Button to Mobile view port

--- src/app/(protected)/settings/integrations/page.tsx
+++ src/app/(protected)/settings/integrations/page.tsx
@@ -47,6 +47,7 @@ export default function SettingsIntegrations() {
                   >
                     App
                   </Button>
+                  {/*  */}
                   <form
                     action="/dashboard"
                     autoComplete="off"
